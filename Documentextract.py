e84999a5fa27fa1250d56c0b041a6c40e2849dce
libin-n-g <libinngeorge@gmail.com>
2017-09-03 16:46:55 +0530

    Added Summary Phrases

--- Documentextract.py
+++ Documentextract.py
@@ -23,6 +23,7 @@ class Sentence(object):
         self.text = text
         self.score = score
         self.words = self.Filterword(self.text)
+        self.SummeryPhrases()
     @staticmethod
     def Filterword(content):
         prepositions = ["to", "from", "by", "is", "that", "a", "as", "of", "In" ]
@@ -35,20 +36,61 @@ class Sentence(object):
         ret = "text= {} \nscore= {} \nwords={}\n".format(self.text, self.score, self.words)
         return ret        
     __repr__ = __str__
-    
+    def SummeryPhrases(self):
+        summ_phra = ["after all",
+                     "all in all",
+                     "all things considered",
+                     "briefly",
+                     "by and large",
+                     "in any case",
+                     "in any event",
+                     "in brief",
+                     "in conclusion",
+                     "on the whole",
+                     "in short",
+                     "in summary",
+                     "in the final analysis",
+                     "in the long run, on balance",
+                     "to sum up",
+                     "to summarize",
+                     "finally"]
+        for phrase in summ_phra:
+            if phrase in self.text:
+                self.score += 5
+    def NamedEntity(self):
+        list1 = []
+        words = self.text.split()
+        if ([word[0].isupper() for word in words]):
+            list1.append(word)
+        return list1
+        
+            
 class Paragraph(object):
     def __init__(self,text):
         self.text = text
         self.sentences = self.FindSentences(text)
+        self.wordset = self.repetitionWord()
     @staticmethod
     def FindSentences(text):
         q = find_all(text, ".")
-        sent = []
+        sen = []
         i_prev = 0
         for i in q:
-            sent.append(Sentence(text[i_prev:i], 10 if i_prev == 0 else 0))
+            sen.append(Sentence(text[i_prev:i], 10 if (i_prev == 0 or i==(len(text)-1)) else 0))
             i_prev = i+1
-        return sent
+        print sen
+        return sen
+    def repetitionWord(self):
+        words = self.text.split()
+        wordset = {}
+        for word in words:
+            if word in wordset:
+                wordset[word]+=1
+            else:
+                wordset[word]=1
+        return wordset
+            
+
 
 if __name__ == "__main__":
     ExtractDocument("Document.txt")
